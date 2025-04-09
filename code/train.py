95a074a913ba7b3ed0ba0e04f87b4f13eb37599b
YK <147385684+beigeb@users.noreply.github.com>
2025-04-09 17:10:18 +0530

    Update train.py

--- code/train.py
+++ code/train.py
@@ -1,9 +1,11 @@
-# code/train.py
 import argparse
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 import joblib
 import os
+import json
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -12,12 +14,26 @@ if __name__ == '__main__':
     parser.add_argument('--train', type=str, default=os.environ.get('SM_CHANNEL_TRAIN'))
     args = parser.parse_args()
 
+    # Load data
     df = pd.read_csv(f'{args.train}/train.csv')
     X = df.drop('Target', axis=1)
     y = df['Target']
 
-    model = RandomForestClassifier()
-    model.fit(X, y)
+    # Train-test split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+    # Train model
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
+
+    # Save model
     joblib.dump(model, os.path.join(args.model_dir, 'model.joblib'))
 
+    # Evaluate
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+
+    # Save metrics
+    metrics = {'accuracy': accuracy}
+    with open(os.path.join(args.output_data_dir, 'evaluation.json'), 'w') as f:
+        json.dump(metrics, f)
